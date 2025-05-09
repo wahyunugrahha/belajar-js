bf1823698d798b721a162892e358059849375413
wahyunugrahha <wahyunih297@gmail.com>
2025-05-09 16:49:24 +0700

    add Quiz JS

---
+++ Quiz-JS-Dicodeng/quiz-3.js
@@ -0,0 +1,22 @@
+const employees = [
+  {
+    name: "Fulan",
+    email: "fulan@dicoding.com",
+    joinYear: 2020,
+  },
+];
+
+function addEmployee(name, email, joinYear) {
+  /**
+   * @TODO
+   * lengkapi fungsi ini agar dapat menambahkan objek employee baru
+   * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
+   */
+  const newEmployee = {
+    name: name,
+    email: email,
+    joinYear: joinYear,
+  };
+  employees.push(newEmployee);
+}
+addEmployee(`Wahyu`, `wahyu@wahyu.com`, 2025);
